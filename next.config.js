d4264807bb46b445c11e2959f1d19b4952d78cc1
KXLAA <kolade.afode@yahoo.com>
2022-12-18 10:12:51 +0000

    feat: add prettier + eslint configs
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    domains: ["ucarecdn.com"],
+  },
+};
+
+module.exports = nextConfig;
