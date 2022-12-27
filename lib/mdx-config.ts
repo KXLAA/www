80bc91d4c632dd3d067e4b9f970c08593348f307
KXLAA <kolade.afode@yahoo.com>
2022-12-27 12:17:55 +0000

    feat: add theme for code block + syntax highlighting
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- lib/mdx-config.ts
+++ lib/mdx-config.ts
@@ -1,6 +1,6 @@
 import type { CompileOptions } from "@mdx-js/mdx";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypePrism from "rehype-prism-plus";
+import rehypePrettyCode, { type Options } from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
 
@@ -8,10 +8,20 @@ type MDXConfig =
   | Omit<CompileOptions, "outputFormat" | "providerImportSource">
   | undefined;
 
+export const rehypePrettyCodeOptions: Partial<Options> = {
+  // use a prepackaged theme
+  theme: "one-dark-pro",
+  // or import a custom theme
+  onVisitHighlightedLine(node) {
+    node.properties.className.push("line--highlighted");
+  },
+};
+
 export const mdxOptions: MDXConfig = {
   remarkPlugins: [remarkGfm],
   rehypePlugins: [
     rehypeSlug,
+    [rehypePrettyCode, rehypePrettyCodeOptions],
     [
       rehypeAutolinkHeadings,
       {
