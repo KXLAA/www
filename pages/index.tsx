cb394aaea1b429340467139f1b3aed0fe2965172
KXLAA <kolade.afode@yahoo.com>
2023-01-01 23:43:58 +0000

    feat: new design on home page
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
 import type { GetStaticProps } from "next";
+import Link from "next/link";
 
-import { Footer } from "@/components/common/Footer";
-import { Layout } from "@/components/common/Layout";
-import { About } from "@/components/home/About";
-import { Posts } from "@/components/home/Posts";
-import { Work } from "@/components/home/Work";
 import type { Post as PostType } from "@/contentlayer/generated";
 import { allPosts } from "@/contentlayer/generated";
 
@@ -13,17 +9,62 @@ type HomeProps = {
 };
 
 export default function Home(props: HomeProps) {
-  const posts = props.posts.slice(0, 4);
+  const posts = props.posts;
 
   return (
-    <Layout className="relative flex-row items-start gap-6 my-24">
-      <div className="flex flex-col gap-10">
-        <About />
-        <Posts posts={posts} />
-        <Work />
-        <Footer />
-      </div>
-    </Layout>
+    <div className="bg-shark-700 main-layout text-silver">
+      <header className="flex justify-between p-10">
+        <Link
+          aria-current="page"
+          className="!text-blue-500 font-medium"
+          href="/"
+        >
+          kxlaa
+        </Link>
+        <nav className="flex gap-4">
+          <Link href="/about">Twitter</Link>
+          <Link href="/work">Linkedin</Link>
+          <Link href="/posts">Email</Link>
+        </nav>
+      </header>
+      <main className="flex flex-col gap-24 px-10 py-16">
+        <div className="flex flex-col gap-0.5">
+          <h1 className="font-bold text-7xl">Kolade Afode</h1>
+          <h2 className="text-3xl !text-blue-500 font-medium">
+            Engineer / Designer
+          </h2>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <h3 className="text-3xl font-bold">Recent Posts</h3>
+          {posts.map((post) => (
+            <Link
+              href={`/posts/${post.slug}`}
+              key={post.slug}
+              className="text-[40px] font-light hover:underline underline-offset-4 decoration-blue-500"
+            >
+              {post.title}
+            </Link>
+          ))}
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <h3 className="text-3xl font-bold">Projects</h3>
+          <a
+            href={`https://www.nartefacts.com/`}
+            className="text-[40px] font-light hover:underline"
+          >
+            Nartefacts
+          </a>
+          <a
+            href={`https://www.devportfolios.dev/`}
+            className="text-[40px] font-light hover:underline"
+          >
+            DevPortfolios
+          </a>
+        </div>
+      </main>
+    </div>
   );
 }
 
