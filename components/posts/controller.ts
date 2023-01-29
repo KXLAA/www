9fb7368c093ee0936bf9475ee6cb152fa0088c97
KXLAA <kolade.afode@yahoo.com>
2023-01-29 18:25:51 +0100

    fix: fix design + clean ups
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

---
+++ components/posts/controller.ts
@@ -0,0 +1,46 @@
+import { useMDXComponent } from "next-contentlayer/hooks";
+
+import type { Post as PostType } from "@/contentlayer/generated";
+import { formatDate } from "@/lib/date";
+import type { MetaProps } from "@/lib/seo";
+
+export function useController(post: PostType) {
+  const Component = useMDXComponent(post.body.code);
+  const path = `/posts/${post.slug}`;
+  const url = `https://kxlaa.com${path}`;
+  const title = `${post.title} | Kola`;
+  const date = post?.publishedAt || new Date().toDateString();
+  const ogImageUrl = `/api/og?title=${encodeURIComponent(
+    post.title
+  )}&date=${encodeURIComponent(formatDate(date, "MMMM dd yyyy"))}`;
+
+  const meta: MetaProps = {
+    title: `${title}`,
+    description: post.description,
+    canonical: url,
+    openGraph: {
+      title,
+      description: post.description,
+      url,
+      type: "article",
+      article: {
+        publishedTime: post.publishedAt,
+        authors: ["https://kxlaa.com"],
+        tags: post?.tags?.map((c: string) => c),
+      },
+      images: [
+        {
+          url: "/images/og-image.png",
+          width: 1200,
+          height: 600,
+          alt: title,
+        },
+      ],
+    },
+  };
+
+  return {
+    Component,
+    meta,
+  };
+}
