a8b8ba4bba43e1f9e6fc09c8f59b23a96df21e66
KXLAA <kolade.afode@yahoo.com>
2023-01-31 14:53:05 +0100

    fix: fix seo title
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -37,7 +37,23 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#101010" />
         <meta name="msapplication-TileColor" content="#101010" />
         <meta name="theme-color" content="#ffffff" />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://kxlaa.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Kola | Full Stack Engineer" />
+        <meta property="og:description" content="Do it with flair ✨" />
+        <meta property="og:image" content="/images/og-image.png" />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="https://kxlaa.com/" />
+        <meta property="twitter:url" content="https://kxlaa.com/" />
+        <meta name="twitter:title" content="Kola | Full Stack Engineer" />
+        <meta name="twitter:description" content="Do it with flair ✨" />
+        <meta name="twitter:image" content="/images/og-image.png" />
       </Head>
+
       <style jsx global>{`
         html {
           font-family: ${poppins.style.fontFamily};
