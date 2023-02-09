c5f0b475a2eb26c51580ea6994c835a11dff5a60
KXLAA <kolade.afode@yahoo.com>
2023-02-09 13:35:36 +0100

    feat: new landing
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

---
+++ components/home/ProjectCard.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+type ProjectCardProps = {
+  title: string;
+  description?: string;
+  link: string;
+  image: string;
+};
+
+export function ProjectCard(props: ProjectCardProps) {
+  const { title, description, link, image } = props;
+  return (
+    <a
+      href={link}
+      className="flex flex-col w-full gap-3 overflow-hidden transition-colors border rounded-md bg-cod-gray-600 border-cod-gray-400 hover:border-cod-gray-200"
+    >
+      <Image src={image} alt={title} width={2000} height={630} />
+      <div className="flex flex-col justify-center gap-1 p-2 text-sm font-light ">
+        <span>{title}</span>
+
+        <span className="text-xs text-silver-700">{description}</span>
+      </div>
+    </a>
+  );
+}
