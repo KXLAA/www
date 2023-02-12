c3dad55d2314b06c6d21a759354c507a33f38d40
KXLAA <kolade.afode@yahoo.com>
2023-02-12 11:34:46 +0100

    fix: fix build issues
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,3 @@
-import * as fs from "node:fs/promises";
-import path from "node:path";
-
-import type { DocumentGen } from "contentlayer/core";
 import { pipe } from "fp-ts/function";
 
 import type {
@@ -15,36 +11,6 @@ export type PostHeading = {
   link?: string;
   level?: number;
 };
-
-export function getHeadings(source: string): PostHeading[] {
-  //get all heading levels from markdown source, including #, ##, ###, etc.
-  const headings = source.match(/#+\s(.*?)\n/g);
-
-  //return array of objects with heading names and ids
-  return (
-    headings?.map((h, i) => {
-      const content = h.match(/#+\s(.*?)\n/)?.[1];
-      const type = headings?.[i].match(/#+/)?.[0];
-      const slug = content?.replace(/ /g, "-").toLowerCase();
-      const link = "#" + slug;
-      return {
-        id: slug || "",
-        content: content || "",
-        link: link || "",
-        level: type?.length || 0,
-      };
-    }) || []
-  );
-}
-
-export const contentDirPath = "content";
-
-export async function getLastEditedDate(doc: DocumentGen): Promise<Date> {
-  const stats = await fs.stat(
-    path.join(contentDirPath, doc._raw.sourceFilePath)
-  );
-  return stats.mtime;
-}
 
 function getPublished<T extends { slug: string }>(content: T[]) {
   return content.filter((c) => !c.slug.startsWith("_"));
