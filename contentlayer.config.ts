0e03d37428f2ded0b83baf69869012c5d71673de
KXLAA <kolade.afode@yahoo.com>
2023-02-12 11:34:46 +0100

    fix: fix build issues
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,3 +1,7 @@
+import * as fs from "node:fs/promises";
+import path from "node:path";
+
+import type { DocumentGen } from "contentlayer/core";
 import {
   defineDocumentType,
   defineNestedType,
@@ -5,8 +9,44 @@ import {
 } from "contentlayer/source-files";
 import readingTime from "reading-time";
 
-import { contentDirPath, getHeadings, getLastEditedDate } from "./lib/api";
 import { mdxOptions } from "./lib/mdx-config";
+
+export const contentDirPath = "content";
+
+type PostHeading = {
+  id?: string;
+  content?: string;
+  link?: string;
+  level?: number;
+};
+
+function getHeadings(source: string): PostHeading[] {
+  //get all heading levels from markdown source, including #, ##, ###, etc.
+  const headings = source.match(/#+\s(.*?)\n/g);
+
+  //return array of objects with heading names and ids
+  return (
+    headings?.map((h, i) => {
+      const content = h.match(/#+\s(.*?)\n/)?.[1];
+      const type = headings?.[i].match(/#+/)?.[0];
+      const slug = content?.replace(/ /g, "-").toLowerCase();
+      const link = "#" + slug;
+      return {
+        id: slug || "",
+        content: content || "",
+        link: link || "",
+        level: type?.length || 0,
+      };
+    }) || []
+  );
+}
+
+async function getLastEditedDate(doc: DocumentGen): Promise<Date> {
+  const stats = await fs.stat(
+    path.join(contentDirPath, doc._raw.sourceFilePath)
+  );
+  return stats.mtime;
+}
 
 const ReadingTime = defineNestedType(() => ({
   name: "readingTime",
