f3fd90006a43496b8d722b1f6aa7c2912e9dc5c7
KXLAA <kolade.afode@yahoo.com>
2023-02-14 16:58:55 +0100

    fix: test absolute url link for  twitter og image
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- components/posts/controller.ts
+++ components/posts/controller.ts
@@ -1,6 +1,7 @@
 import { useMDXComponent } from "next-contentlayer/hooks";
 
 import type { Post as PostType } from "@/contentlayer/generated";
+import { getBaseUrl } from "@/lib/get-base-url";
 import type { MetaProps } from "@/lib/seo";
 
 export function useController(post: PostType) {
@@ -8,6 +9,7 @@ export function useController(post: PostType) {
   const path = `/posts/${post.slug}`;
   const url = `https://kxlaa.com${path}`;
   const title = `Writing | ${post.title}`;
+  const ogImage = `${getBaseUrl()}/${post.ogImage}}`;
 
   const meta: MetaProps = {
     title: title,
@@ -25,7 +27,7 @@ export function useController(post: PostType) {
       },
       images: [
         {
-          url: post.ogImage || "/images/main-og-image.jpg",
+          url: ogImage || "/images/main-og-image.jpg",
           width: 1200,
           height: 600,
           alt: title,
