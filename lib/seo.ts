3c26078ff6a709bd97993db919bdcd91dd42450a
KXLAA <kolade.afode@yahoo.com>
2023-02-14 09:42:58 +0100

    fix: fix seo
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- lib/seo.ts
+++ lib/seo.ts
@@ -9,12 +9,11 @@ export const seo: DefaultSeoProps = {
     title: `Kola | Full Stack Engineer`,
     type: "website",
     url: `https://kxlaa.com`,
-    site_name: `Kola | Full Stack Engineer`,
     images: [
       {
-        url: "/images/og-image.png",
+        url: "/images/main-og-image.png",
         width: 1200,
-        height: 630,
+        height: 600,
         alt: `Kola | Full Stack Engineer`,
       },
     ],
