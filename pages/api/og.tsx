5b40475f28d1d090db80d758abdd6a26a1938028
KXLAA <kolade.afode@yahoo.com>
2023-02-16 19:31:07 +0100

    fix: new og image options
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -26,25 +26,18 @@ export default function (req: NextRequest) {
 
     return new ImageResponse(
       (
-        // Modified based on https://tailwindui.com/components/marketing/sections/cta-sections
-        <div
-          tw="w-full h-full flex flex-col text-center items-center justify-between py-11"
-          style={{
-            backgroundImage:
-              "radial-gradient(circle at 25% 25%, #191d21 0%, #605e6755 15%)",
-            backgroundSize: "50px 50px",
-            maskImage: "radial-gradient(black 0.9px, transparent 0.9px)",
-            maskSize: "18px 18px",
-          }}
-        >
-          <div tw="flex flex-col items-center w-full">
-            <div tw="flex flex-col p-8 w-full h-full justify-between">
-              <div tw="flex flex-col justify-center items-start w-full p-8 self-end rounded-xl">
-                <div tw="flex text-white">Check Out This Article</div>
-                <div tw="flex text-4xl font-bold text-white">{title}</div>
-                <div tw="flex font-bold text-white">Kxlaa.com | {date}</div>
-              </div>
-            </div>
+        <div tw="w-full h-full bg-[#111111] flex flex-col justify-center items-center relative">
+          <div tw="flex items-center justify-center -mt-40">
+            <div tw="w-[148px] h-[148px] bg-[#282828] ml-4" />
+            <div tw="w-[148px] h-[148px] bg-[#1C1C1C] ml-4" />
+            <div tw="w-[148px] h-[148px] bg-[#161616] ml-4" />
+          </div>
+          <div tw="w-full bg-[#161616] p-14 absolute bottom-0 flex gap-3 flex-col">
+            <span tw="font-black text-[#464646] text-xl">ARTICLE #01 </span>
+            <h1 tw="text-[#999999] font-semibold text-5xl">
+              Drag & Drop in React with Dnd Kit
+            </h1>
+            <span tw="text-[#757575] font-light">February 15, 2023</span>
           </div>
         </div>
       ),
