5be32b8332f5f6f168b5a1a164823d6711d914c7
KXLAA <kolade.afode@yahoo.com>
2023-02-17 13:13:51 +0100

    fix: refactor api into class
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,14 +1,13 @@
 import type { PostsPageProps } from "@/components/posts/PostsPage";
 import { PostsPage } from "@/components/posts/PostsPage";
-import { allPosts } from "@/contentlayer/generated";
-import { prepare } from "@/lib/api";
+import { api } from "@/lib/api";
 
 export default (props: PostsPageProps) => <PostsPage {...props} />;
 
 export const getStaticProps = async () => {
   return {
     props: {
-      posts: prepare.posts(allPosts),
+      posts: api.getMinimalPosts(),
     },
   };
 };
