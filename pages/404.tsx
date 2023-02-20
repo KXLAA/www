c936ced1a619b2de6b6f4bc938bf51588fef160b
KXLAA <kolade.afode@yahoo.com>
2023-02-20 19:06:48 +0100

    fix: more fixes to 4040 page
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,17 @@
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 import { Layout } from "@/components/common/Layout";
+import { cx } from "@/lib/cx";
 
 export default () => (
-  <Layout className="flex flex-col justify-center max-w-xl gap-4 px-4 py-4 text-base md:px-10 md:py-16 md:gap-8 md:text-xl font-extralight">
+  <Layout
+    className="flex flex-col justify-center max-w-xl gap-4 px-4 py-4 text-base md:px-10 md:py-16 md:gap-8 md:text-xl font-extralight"
+    customMeta={{
+      title: "404: The page you are looking for does not exist",
+      description: "The page you are looking for does not exist.",
+    }}
+  >
     <div className="flex flex-col ">
       <Link
         href="/"
@@ -13,18 +21,17 @@ export default () => (
       </Link>
       <p className="text-2xl font-semibold underline">Page not found </p>
 
-      <p className="mt-1 text-sm text-silver-600">
-        The page you are looking for does not exist.
-      </p>
-      <p className="mt-1 text-sm text-silver-700">
-        The page you are looking for does not exist.
-      </p>
-      <p className="mt-1 text-sm text-silver-800">
-        The page you are looking for does not exist.
-      </p>
-      <p className="mt-1 text-sm text-silver-900">
-        The page you are looking for does not exist.
-      </p>
+      {Array.from({ length: 4 }).map((_, i) => (
+        <motion.p
+          key={i}
+          className={cx("mt-1 text-sm", `text-silver-${600 + i * 100}`)}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.1 + i * 0.1 }}
+        >
+          The page you are looking for does not exist.
+        </motion.p>
+      ))}
     </div>
   </Layout>
 );
