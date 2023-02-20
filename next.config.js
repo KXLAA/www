bc9e4548645996fd0ec5f4cd176a6176c985f779
KXLAA <kolade.afode@yahoo.com>
2023-02-20 18:11:16 +0100

    feat: add some analytics to site
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- next.config.js
+++ next.config.js
@@ -7,6 +7,18 @@ const nextConfig = {
   images: {
     domains: ["ucarecdn.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+    ];
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
