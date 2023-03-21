9cb64819ba9cfbc32c9f7d83e1926eaf83702fb4
KXLAA <kolade.afode@yahoo.com>
2023-03-21 07:37:31 +0000

    fix: more fixes to base url function
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- lib/get-base-url.ts
+++ lib/get-base-url.ts
@@ -1,3 +1,7 @@
 export function getBaseUrl() {
-  return process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
+  if (process.env.NODE_ENV === "development") {
+    return "http://localhost:3000";
+  }
+
+  return process.env.NEXT_PUBLIC_SITE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL;
 }
