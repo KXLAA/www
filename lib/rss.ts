f8d648d85b2346dce9067ffac0d4367ed454e660
KXLAA <kolade.afode@yahoo.com>
2023-03-21 08:49:59 +0000

    fix: more improvements to rss
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- lib/rss.ts
+++ lib/rss.ts
@@ -1,30 +1,51 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { parseISO } from "date-fns";
-import { promises as fs } from "fs";
-import RSS from "rss";
+import { Feed } from "feed";
+import fs from "fs";
 
 import { api } from "@/lib/api";
 
 export default async function generateRSS() {
-  const feed = new RSS({
-    title: "Kola | Frontend Engineer",
-    description: "Do it with flair ✨",
-    site_url: "https://www.kxlaa.com",
-    feed_url: "https://www.kxlaa.com/feed.xml",
-    language: "en",
-    pubDate: new Date(),
-    copyright: `All rights reserved ${new Date().getFullYear()}, Kola`,
+  const date = new Date();
+  const author = {
+    name: "Kolade Afode",
+    email: "Koladeboss@icloud.com",
+    link: "https://twitter.com/kxlaa_",
+  };
+
+  const feed = new Feed({
+    title: "Kola | Articles",
+    description: "",
+    id: "https://www.kxlaa.com",
+    link: "https://www.kxlaa.com",
+    image:
+      "https://ucarecdn.com/2fecb5b1-ed81-47b7-917d-b5c70f3ed733/mainogimage.jpg",
+    favicon: `/favicon-32x32.png`,
+    copyright: `All rights reserved ${date.getFullYear()}, Kola`,
+    updated: date,
+    generator: "Feed for Node.js",
+    feedLinks: {
+      rss2: `/rss/feed.xml`,
+      json: `/rss/feed.json`,
+      atom: `/rss/atom.xml`,
+    },
+    author,
   });
 
   api.posts.published.forEach((post) => {
-    feed.item({
+    feed.addItem({
       title: post.title,
-      url: `https://kxlaa.com/posts/${post._raw.flattenedPath}`,
+      id: `https://kxlaa.com/posts/${post._raw.flattenedPath}`,
+      link: `https://kxlaa.com/posts/${post._raw.flattenedPath}`,
       date: parseISO(post.publishedAt),
       description: post.description,
-      author: "Kolade Afode",
+      content: post.description,
+      author: [author],
+      contributor: [author],
     });
   });
 
-  await fs.writeFile("./public/rss.xml", feed.xml({ indent: true }));
+  fs.mkdirSync("./public/rss", { recursive: true });
+  fs.writeFileSync("./public/rss/feed.xml", feed.rss2());
+  fs.writeFileSync("./public/rss/atom.xml", feed.atom1());
+  fs.writeFileSync("./public/rss/feed.json", feed.json1());
 }
