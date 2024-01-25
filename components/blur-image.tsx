821ff71c1721b528eda7dcf6a32085f4f6510ef3
KXLAA <kolade.afode@yahoo.com>
2024-01-25 08:31:41 +0000

    fix: rounded-full to blur loader
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- components/blur-image.tsx
+++ components/blur-image.tsx
@@ -2,6 +2,7 @@
 
 import Image, { ImageProps } from "next/image";
 import { useState, useEffect } from "react";
+import { cx } from "@/lib/cx";
 
 export function BlurImage(props: ImageProps) {
   const [isLoading, setLoading] = useState(true);
@@ -13,11 +14,13 @@ export function BlurImage(props: ImageProps) {
       {...props}
       src={src}
       alt={props.alt}
-      className={`${props.className} transition-all ${
+      className={cx(
+        "transition-all rounded-full",
         isLoading
           ? "grayscale blur-lg scale-100"
-          : "grayscale-0 blur-0 scale-100"
-      }`}
+          : "grayscale-0 blur-0 scale-100",
+        props.className
+      )}
       placeholder="blur"
       blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAMAAAADCAYAAABWKLW/AAAAAXNSR0IArs4c6QAAADJJREFUGFcBJwDY/wHS/+3/3QDHAO4A+wABxv/d/9TzwQDxAgcAAaX/u//Tt8gA9P0OAMiwFiDLe9QcAAAAAElFTkSuQmCC"
       onLoadingComplete={async () => {
