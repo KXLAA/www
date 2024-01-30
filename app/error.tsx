7b06fd06e93ea88653e2afc47a2f13ded6c0864f
KXLAA <kolade.afode@yahoo.com>
2024-01-30 17:29:36 +0000

    feat: error & 404 pages
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import React from "react";
+import NextLink from "next/link";
+import { BlurImage } from "@/components/blur-image";
+import { MainLayout } from "@/components/layout";
+import { cx } from "@/lib/cx";
 
 interface Props {
   error: Error;
@@ -9,13 +13,33 @@ interface Props {
 
 export default function Error({ error, reset }: Props) {
   React.useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error);
   }, [error]);
 
   return (
-    <div>
-      <p>Oh no, something went wrong... maybe refresh?</p>
-    </div>
+    <MainLayout>
+      <NextLink href="/">
+        <BlurImage
+          src="/common/kola-memoji-peeking.png"
+          alt="Kola's profile picture"
+          width="150"
+          height="150"
+        />
+      </NextLink>
+
+      <h2 className="md:text-5xl text-4xl font-bold leading-relaxed">
+        Oh no, something went wrong... maybe{" "}
+        <button
+          className={cx(
+            "underline decoration-dashed decoration-green-10 transition-all duration-150  underline-offset-2",
+            "hover:text-green-10 hover:decoration-green-11 w-fit"
+          )}
+          onClick={reset}
+        >
+          refresh
+        </button>
+        ?
+      </h2>
+    </MainLayout>
   );
 }
